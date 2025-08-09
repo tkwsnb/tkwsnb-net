605f5b3800e4c8b47874bc5e50121119c641b295
tkwsnb <ishigesato@gmail.com>
2025-08-09 12:16:14 +0900

    og-imageのタイトルフォントを「M PLUS 1p」に変更し、日本語テキストの表示を改善しました。

--- src/pages/og-image/[...slug].png.ts
+++ src/pages/og-image/[...slug].png.ts
@@ -34,7 +34,7 @@ const markup = (title: string, pubDate: string) =>
 	html`<div tw="flex flex-col w-full h-full bg-[#1d1f21] text-[#c9cacc]">
 		<div tw="flex flex-col flex-1 w-full p-10 justify-center">
 			<p tw="text-2xl mb-6">${pubDate}</p>
-			<h1 tw="text-6xl font-bold leading-snug text-white" style="font-family: 'Noto Sans JP', 'Roboto Mono'">${title}</h1>
+			<h1 tw="text-6xl font-bold leading-snug text-white" style="font-family: 'M PLUS 1p', 'Noto Sans JP', 'Roboto Mono'">${title}</h1>
 		</div>
 		<div tw="flex items-center justify-between w-full p-10 border-t border-[#2bbc89] text-xl">
 			<div tw="flex items-center">
